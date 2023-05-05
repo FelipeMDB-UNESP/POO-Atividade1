4a2190d02e5b2f495ffac35bf784ee2f16a310fe
Sortudo13 <106319746+Sortudo13@users.noreply.github.com>
2023-05-05 20:38:03 -0300

    Add classe livro

--- grupo6/src/main/java/br/unesp/rc/classes/Livro.java
+++ grupo6/src/main/java/br/unesp/rc/classes/Livro.java
@@ -10,4 +10,66 @@ package br.unesp.rc.classes;
  */
 public class Livro {
     
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private String editora;
+    private long id;
+    // Construtor
+    public Livro(String titulo, String autor, int anoPublicacao, String editora, long id) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.editora = editora;
+        this.id = id;
+    }
+    
+    
+    
+    
+    
+    //Getters and Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    
+    
+    
 }

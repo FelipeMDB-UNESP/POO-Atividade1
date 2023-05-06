2952175d502a2a7558bd8203ad9151bd261d7e3e
Sortudo13 <106319746+Sortudo13@users.noreply.github.com>
2023-05-05 21:32:29 -0300

    Add files via upload

---
+++ grupo6/src/main/java/br/unesp/rc/classes/Usuario.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unesp.rc.classes;
+
+/**
+ *
+ * @author aluno
+ */
+public class Usuario {
+    private String nome;
+    private long cpf;
+    private long telefone; 
+    private String endereco;
+    
+    
+    //getters and setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+}

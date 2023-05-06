4c5e2338a2478f098f1ccd09a1ac00e744083653
Felipe <105891789+FelipeMDB-UNESP@users.noreply.github.com>
2023-05-05 21:37:46 -0300

    Add files via upload

--- grupo6/src/main/java/br/unesp/rc/classes/ServicosBiblioteca.java
+++ grupo6/src/main/java/br/unesp/rc/classes/ServicosBiblioteca.java
@@ -4,28 +4,47 @@
  */
 package br.unesp.rc.classes;
 
+import java.util.Date;
+
 /**
  *
  * @author aluno
  */
 public class ServicosBiblioteca {
     
-    public static void registrarEmprestimo(Emprestimo emp, String nome){
+    public static void registrarEmprestimo(Emprestimo emprestimo, String nomeUsuario){
         
-        System.out.println("Um novo emprestimo foi gerado para:" + nome);
+        System.out.println("Um novo emprestimo foi gerado para:" + nomeUsuario + "de id:" + emprestimo.getIdCliente());
         System.out.println("Os seguintes livros foram emprestados: ");
-        for(Livro l : emp.livrosEmprestados){
-            System.out.println(Long.toString(l.id))
+        for(Livro l : emprestimo.livrosEmprestados){
+            System.out.println(Long.toString(l.getId()) + ":" + l.getTitulo());
         }
+        System.out.println("Data do empréstimo: " + emprestimo.getDataRetirada());
+        System.out.println("Data da devolução: " + emprestimo.getDataDevolucao());
     }
     
-    public static boolean devolverEmprestimo(Emprestimo){
-        haAtraso
-    }
-    
-    public static boolean haAtraso(Emprestimo emprestimo){
+    public static void devolverEmprestimo(Emprestimo emprestimo, String nomeUsuario){
+        System.out.println("Os seguintes livros foram devolvidos: ");
+        for(Livro l : emprestimo.livrosEmprestados){
+            System.out.println(Long.toString(l.getId()) + ":" + l.getTitulo());
+        }
         
-        return false;
+        if(houveAtraso(emprestimo) > 0){
+            System.out.println("O usuário " + nomeUsuario + "deverá pagar o valor de R$" + houveAtraso(emprestimo));
+        }
+        else{
+            System.out.println("O usuário " + nomeUsuario + "fez a devolução dentro da data prevista");
+        }
+        System.out.println("Emissão do comprovante de devolução");
+    }
+    
+    public static int houveAtraso(Emprestimo emprestimo){
+        
+        Date hoje = new Date(); // captura a data de hoje
+        if(emprestimo.dataDevolucao.compareTo(hoje) < 0 ){ // se < 0 dataDevolucao ocorre antes da data atual
+            return (emprestimo.getLivrosEmprestados().size() * 2);
+        }
+        return 0;
     }
     
 }
